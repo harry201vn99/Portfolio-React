128750e92064735dbf48a253ea0a89016c49177a
Harry Nguyen <harrynguyen12099vn@outlook.com>
2024-04-14 17:39:28 +1000

    Create and Move CSS BG Animations component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import Sidenav from './components/Sidenav'
 import Main from './components/Main'
-import About from './components/About'
 
 function App() {
     return (
         <div className="dark:bg-gray-800 bg-white" id="bgcolor">
             <Sidenav />
             <Main />
-            <About />
         </div>
     )
 }
